8d41636ea07aba1030b736e07c2447594f63db24
LordAnthrax94 <andreagi1994@gmail.com>
2025-05-07 18:44:55 +0200

    Fix: inserita risposta delle promises

--- cityDash.js
+++ cityDash.js
@@ -13,4 +13,18 @@ async function getDashboardData (query){
   
 }
 
+// (async () =>{
+  
+// })();
+
+getDashboardData('london')
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.city} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error));
 
